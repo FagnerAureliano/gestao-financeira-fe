721b8d96e06b82532f100d713038e29b14223f78
FagnerAureliano <fagner2aurelian@gmail.com>
2022-05-05 15:27:49 -0300

    implementacao login

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,10 @@
-import { Formik, Field, Form, ErrorMessage } from "formik";
-import "./App.css";
+import Login from "./modules/login/components/login.component";
 
 function App() {
-  const initialValues = {
-    email: "",
-    password: "",
-  };
-
-  function onSubmit(values: any) {
-    console.log(values)
-  }
-
   return (
     <div className="App">
       <header className="App-header">
-        <p>Project Gestão Financeira </p>
-
-        <Formik
-          initialValues={initialValues}
-          onSubmit={onSubmit}
-
-          render={({ values }) => (
-            <Form>
-              <div>
-                <div>
-                  <label>Username</label>
-                </div>
-                <div>
-                  <Field type="email" name="email" />
-
-                  <ErrorMessage name="email" component="div" />
-
-                </div>
-              </div>
-              <div>
-                <div>
-                  <label>Password</label>
-                </div>
-                <div>
-                  <Field type="password" name="password"  />
-                </div>
-              </div>
-              <button type="submit" > Submit </button>
-            </Form>
-          )}
-        />
+        <Login />
       </header>
     </div>
   );
--- src/modules/login/components/login.component.tsx
+++ src/modules/login/components/login.component.tsx
@@ -1,30 +1,52 @@
-import { Component } from "react";
- 
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-    RouterProps,
-  } from "react-router-dom";
-  
-type Props =  RouterProps 
-type State = {
-    username: string,
-    password: string,
-    loading: boolean,
-    message: string
-  };
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import "../../../App.css";
 
-export default class Login extends Component<Props, State>{
+import validationSchema from "./schema";
 
-    // handleLogin(formValue: { username: string; password: string }) {
-    //     const { username, password } = formValue;
-    
-    //     this.setState({
-    //       message: "",
-    //       loading: true
-    //     });
-    // }
+const initialValues = {
+  email: "",
+  password: "",
+};
 
+function onSubmit(values: any) {
+  console.log(values);
+}
 
+export default function Login() {
+  return (
+    <div className="App"> 
+      <header className="App-header">
+        {/* <h3>Heiser Fitness</h3> */}
+        <label>Gestão Financeira</label>
+        <br />
+        <Formik
+          initialValues={initialValues}
+          onSubmit={onSubmit}
+          validationSchema={validationSchema}
+        >
+          <Form>
+            <div>
+              <div>
+                <small>Username</small>
+              </div>
+              <div>
+                <Field type="email" name="email" />
+                <ErrorMessage name="email" component="div" />
+              </div>
+            </div>
+            <div>
+              <div>
+                <small>Password</small>
+              </div>
+              <div>
+                <Field type="password" name="password" />
+                <ErrorMessage name="password" component="div" />
+              </div>
+            </div>
+            <button type="submit" > LOGIN </button>
+          </Form>
+        </Formik>
+      </header>
+    </div>
+  );
 }
