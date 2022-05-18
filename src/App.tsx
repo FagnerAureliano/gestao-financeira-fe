6c328def2cc93fee82ae5ac9a3a64142419ceb56
fagneraureliano <fagner2aureliano@gmail.com>
2022-05-17 21:07:32 -0300

    fix layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
 import { Login } from "./modules/login/components/Login";
 
 export function App() {
-  return (
-
-    <div className="bg-slate-800 flex justify-center items-center h-[70vw] w-[100vw] md:w-auto">
-      <div className="text-slate-400">
-        <Login />
-      </div>
-    </div>
-  );
+  return <Login />;
 }
--- src/modules/login/components/Login.tsx
+++ src/modules/login/components/Login.tsx
@@ -1,4 +1,6 @@
 import { Formik, Field, Form, ErrorMessage } from "formik";
+import { LockClosedIcon } from "@heroicons/react/solid";
+import  financeIcon from "./../../../assets/financeIcon.png";
 
 import validationSchema from "./schema";
 
@@ -7,48 +9,105 @@ const initialValues = {
   password: "",
 };
 
-function onSubmit(values: any) {
+function handleSigIn(values: any) {
   console.log(values);
 }
 
-
 export function Login() {
   return (
-    <div className="App"> 
-      <header className="App-header">
-        {/* <h3>Heiser Fitness</h3> */}
-        <label className="">Gestão Financeira</label>
-        
+    <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <img
+            className="mx-auto h-[70px] w-auto"
+            src={financeIcon}
+            alt="Workflow"
+          />
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Gestão Financeira
+          </h2>
+          <h4 className="mt-3 text-center text-1xl font-extrabold text-gray-900">
+            Sign in to your account
+          </h4>
+        </div>
         <Formik
           initialValues={initialValues}
-          onSubmit={onSubmit}
+          onSubmit={handleSigIn}
           validationSchema={validationSchema}
         >
-          <Form>
-            <div>
+          <Form className="mt-8 space-y-6" method="POST">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="rounded-md shadow-sm -space-y-px">
               <div>
-                <small>Username</small>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <Field
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Email address"
+                />
               </div>
               <div>
-                <Field className="rounded-sm" type="email" name="email" />
-                <ErrorMessage className="text-yellow-300 text-sm" name="email" component="div" />
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <Field
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Password"
+                />
               </div>
             </div>
-            <div>
-              <div>
-                <small>Password</small>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <Field
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="ml-2 block text-sm text-gray-900"
+                >
+                  Remember me
+                </label>
               </div>
-              <div>
-                <Field className="rounded-sm" type="password" name="password" />
-                <ErrorMessage className="text-yellow-300 text-sm"  name="password" component="div" />
+
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-medium text-indigo-600 hover:text-indigo-500"
+                >
+                  Forgot your password?
+                </a>
               </div>
             </div>
-            <div className="">
-            <button className="bg-green-600 hover:bg-green-500 rounded-lg px-20 h-6 transition-colors text-white flex m-4 items-center group" type="submit" > LOGIN </button>
+
+            <div>
+              <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                  <LockClosedIcon
+                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                    aria-hidden="true"
+                  />
+                </span>
+                Sign in
+              </button>
             </div>
           </Form>
         </Formik>
-      </header>
-    </div>
+      </div>
+    </div> 
   );
 }
